c162eabf745e7468365f83c08e5f8f495193d68a
Rodrigo Dojo <rodrigodoj@gmail.com>
2022-05-08 17:44:05 -0300

    projeto inicial

---
+++ src/main/java/com/dojo/booksafe/BooksafeApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.booksafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojo/booksafe/BooksafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.booksafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
